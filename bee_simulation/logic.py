5a263108787845820c2fa89c8d9833d33d91d3ed
Roeland Oostdam <Roeland.Oostdam@outlook.com>
2021-10-13 21:20:03 +0200

    fetch_nectar overwrites grid_values

--- bee_simulation/logic.py
+++ bee_simulation/logic.py
@@ -42,17 +42,21 @@ def update_state(agent):
 
 
 def calc_grid_scores(agent):
-    grid_cost_base = helpers.generate_grid_costs(agent, nexus_pos=agent.hive_pos)
-    grid_cost_cpos = helpers.generate_grid_costs(agent, nexus_pos=agent.pos, from_agent=True)
-    grid_gain_gain = helpers.generate_grid_gain(agent, clue_loc=agent.clue_loc, clue_grade=agent.clue_grade)
+    if agent.state == 'fetch_nectar':
+        grid_scores = np.zeros([agent.model.width, agent.model.height], dtype=np.float)
+        grid_scores[agent.clue_loc] = 10000
+    else:
+        grid_cost_base = helpers.generate_grid_costs(agent, nexus_pos=agent.hive_pos)
+        grid_cost_cpos = helpers.generate_grid_costs(agent, nexus_pos=agent.pos, from_agent=True)
+        grid_gain_gain = helpers.generate_grid_gain(agent, clue_loc=agent.clue_loc, clue_grade=agent.clue_grade)
 
-    grid_scores = - grid_cost_base - grid_cost_cpos + grid_gain_gain
-    grid_scores = np.clip(grid_scores, -1000, 1000)
+        grid_scores = - grid_cost_base - grid_cost_cpos + grid_gain_gain
+        grid_scores = np.clip(grid_scores, -1000, 1000)
 
-    # print("grid_cost_base:\n",np.rot90(grid_cost_base))
-    # print("grid_cost_cpos:\n",np.rot90(grid_cost_cpos))
-    # print("grid_gain_wclue:\n",np.rot90(grid_gain_gain))
-    # print("grid_scores:\n", np.rot90(grid_scores))
+        # print("grid_cost_base:\n",np.rot90(grid_cost_base))
+        # print("grid_cost_cpos:\n",np.rot90(grid_cost_cpos))
+        # print("grid_gain_wclue:\n",np.rot90(grid_gain_gain))
+        # print("grid_scores:\n", np.rot90(grid_scores))
     return grid_scores
 
 
